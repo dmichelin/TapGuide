f4d9f088b07e45a9cb24b1d2e63e522d4e073844
Daniel Michelin <k14dm01@kzoo.edu>
2017-11-30 01:12:17 -0500

    cleanup

--- app/src/main/java/tech/danielmichelin/tapguide/Screens/TripOverviewScreen/TripOverviewActivity.kt
+++ app/src/main/java/tech/danielmichelin/tapguide/Screens/TripOverviewScreen/TripOverviewActivity.kt
@@ -84,59 +84,8 @@ class TripOverviewActivity: AppCompatActivity() {
             val activityType = v?.findViewById<TextView>(R.id.description)
             activityType?.text = business.eventType
 
-            // check this shit out
-            v?.setOnTouchListener(object: OnSwipeTouchListener(context) {
-                override fun onSwipeLeft() {
-                    Toast.makeText(context,"YOU SWIPED LEFT",Toast.LENGTH_LONG).show()
-                }
-            });
             return v
         }
 
-    }
-
-    /**
-     * Detects left and right swipes across a view.
-     *
-     * https://stackoverflow.com/questions/4139288/android-how-to-handle-right-to-left-swipe-gestures
-     */
-    open inner class OnSwipeTouchListener(context: Context) : View.OnTouchListener {
-
-        private val gestureDetector: GestureDetector
-
-        init {
-            gestureDetector = GestureDetector(context, GestureListener())
-        }
-
-        open fun onSwipeLeft() {}
-
-        open fun onSwipeRight() {}
-
-        override fun onTouch(v: View, event: MotionEvent): Boolean {
-            return gestureDetector.onTouchEvent(event)
-        }
-
-        private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
-            private val SWIPE_DISTANCE_THRESHOLD = 100
-            private val SWIPE_VELOCITY_THRESHOLD = 100
-
-            override fun onDown(e: MotionEvent): Boolean {
-                return true
-            }
-
-            override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
-                val distanceX = e2.x - e1.x
-                val distanceY = e2.y - e1.y
-                if (Math.abs(distanceX) > Math.abs(distanceY) && Math.abs(distanceX) > SWIPE_DISTANCE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                    if (distanceX > 0)
-                        onSwipeRight()
-                    else
-                        onSwipeLeft()
-                    return true
-                }
-                return false
-            }
-
-        }
     }
 }
