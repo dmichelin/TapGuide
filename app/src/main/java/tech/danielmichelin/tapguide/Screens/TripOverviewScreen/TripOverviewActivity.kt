a06775df8a30f34f4ef75d9b3a0d45e4234a0314
Daniel Michelin <k14dm01@kzoo.edu>
2017-11-30 01:43:51 -0500

    added library

--- app/src/main/java/tech/danielmichelin/tapguide/Screens/TripOverviewScreen/TripOverviewActivity.kt
+++ app/src/main/java/tech/danielmichelin/tapguide/Screens/TripOverviewScreen/TripOverviewActivity.kt
@@ -2,29 +2,21 @@ package tech.danielmichelin.tapguide.Screens.TripOverviewScreen
 
 import android.animation.ObjectAnimator
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
+import android.graphics.Color
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
-import android.view.GestureDetector
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewTreeObserver
 import android.widget.*
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.squareup.picasso.Picasso
 import com.willowtreeapps.spruce.Spruce
 import com.willowtreeapps.spruce.animation.DefaultAnimations
 import com.willowtreeapps.spruce.sort.DefaultSort
-import com.yelp.fusion.client.models.Business
 import tech.danielmichelin.tapguide.R
-import tech.danielmichelin.tapguide.Enums.ActivityType
 import tech.danielmichelin.tapguide.Model.TGBusiness
-import android.support.v4.view.GestureDetectorCompat
-import android.view.MotionEvent
-import android.text.method.Touch.onTouchEvent
 import android.util.Log
+import org.qap.ctimelineview.TimelineRow
+import org.qap.ctimelineview.TimelineViewAdapter
 
 
 /**
@@ -38,10 +30,30 @@ class TripOverviewActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(View.inflate(this, R.layout.activity_trip_overview,null))
         listView = findViewById<ListView>(R.id.activity_list)
-        val businesses = intent.extras.get("businesses")
-        listView.viewTreeObserver.addOnGlobalLayoutListener({initSpruce()})
 
-        listView.adapter = BusinessAdapter(this,businesses as Array<TGBusiness>)
+        val businesses = intent.extras.get("businesses") as Array<TGBusiness>
+
+        // implement timelineview
+        val timelineRowsList = ArrayList<TimelineRow>()
+
+        for(i in 0..businesses.size-1){
+            val business = businesses.get(i)
+            val row = TimelineRow(i)
+            row.setImageSize(40);
+            row.setBelowLineColor(Color.argb(255, 0, 0, 0));
+// To set row Below Line Size in dp (optional)
+            row.setBelowLineSize(6);
+
+            row.title= business.eventType
+            row.description = business.name
+            row.image = business.imageUrl
+
+            timelineRowsList.add(row)
+        }
+
+        //listView.viewTreeObserver.addOnGlobalLayoutListener({initSpruce()})
+
+        listView.adapter = TimelineViewAdapter(this,0,timelineRowsList,false)
         Log.d("Test", "Test")
 
         // Instantiate the gesture detector with the
@@ -74,8 +86,7 @@ class TripOverviewActivity: AppCompatActivity() {
 
 
 
-            if (!business.imageUrl.equals(""))
-                Picasso.with(context).load(business.imageUrl).resize(300, 300).centerCrop().into(image)
+
             val name = v?.findViewById<TextView>(R.id.businessName)
             name?.text = business.name
             val activityType = v?.findViewById<TextView>(R.id.description)
