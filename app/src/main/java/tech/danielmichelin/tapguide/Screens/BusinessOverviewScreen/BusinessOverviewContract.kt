cb92f472c8d5d04f762d31c623e32a57e41ba5b1
Daniel Michelin <k14dm01@kzoo.edu>
2018-01-29 20:07:54 -0500

    added the beginning of an overview screen

---
+++ app/src/main/java/tech/danielmichelin/tapguide/Screens/BusinessOverviewScreen/BusinessOverviewContract.kt
@@ -0,0 +1,8 @@
+package tech.danielmichelin.tapguide.Screens.BusinessOverviewScreen
+
+/**
+ * Created by Daniel on 1/29/2018.
+ */
+interface BusinessOverviewPresenter
+
+interface BusinessOverviewView
